c0619390fc7336a8e5db113277f4534aa7ea5048
~JAY GOGA~ <dkpatel3116@gmail.com>
2024-05-31 19:37:10 +0530

    Initial commit

---
+++ lib/Carousal_Slide/view/carousal_slide_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CarousalSlide extends StatelessWidget {
+  const CarousalSlide({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'Carousal_Slide/view/carousal_slide_screen.dart';
+
+void main() {
+  runApp(const CarousalSlide());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
+
+
